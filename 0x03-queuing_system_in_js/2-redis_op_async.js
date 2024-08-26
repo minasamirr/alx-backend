7dda74497f1dba352832dcdd487d280714d1829f
minasamirr <mina430q@gmail.com>
2024-08-26 18:38:38 +0300

    .

---
+++ 0x03-queuing_system_in_js/2-redis_op_async.js
@@ -0,0 +1,37 @@
+#!/usr/bin/yarn dev
+import redis from 'redis';
+import { promisify } from 'util';
+
+// Create a Redis client
+const client = redis.createClient();
+
+// Event listener for successful connection
+client.on('connect', () => {
+  console.log('Redis client connected to the server');
+});
+
+// Event listener for errors
+client.on('error', (err) => {
+  console.log(`Redis client not connected to the server: ${err.message}`);
+});
+
+// Promisify the get function
+const getAsync = promisify(client.get).bind(client);
+
+function setNewSchool(schoolName, value) {
+  client.set(schoolName, value, redis.print);
+}
+
+async function displaySchoolValue(schoolName) {
+  try {
+    const value = await getAsync(schoolName);
+    console.log(value);
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+// Test the functions
+displaySchoolValue('Holberton');
+setNewSchool('HolbertonSanFrancisco', '100');
+displaySchoolValue('HolbertonSanFrancisco');

f3ac686816a8e73b2c194751bfe6fc474163aee7
minasamirr <mina430q@gmail.com>
2024-08-26 18:32:26 +0300

    task 0 1

---
+++ 0x03-queuing_system_in_js/1-redis_op.js
@@ -0,0 +1,34 @@
+#!/usr/bin/yarn dev
+import redis from 'redis';
+
+// Create a Redis client
+const client = redis.createClient();
+
+// Event listener for successful connection
+client.on('connect', () => {
+  console.log('Redis client connected to the server');
+});
+
+// Event listener for errors
+client.on('error', (err) => {
+  console.log(`Redis client not connected to the server: ${err.message}`);
+});
+
+function setNewSchool(schoolName, value) {
+  client.set(schoolName, value, redis.print);
+}
+
+function displaySchoolValue(schoolName) {
+  client.get(schoolName, (err, res) => {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log(res);
+    }
+  });
+}
+
+// Test the functions
+displaySchoolValue('Holberton');
+setNewSchool('HolbertonSanFrancisco', '100');
+displaySchoolValue('HolbertonSanFrancisco');
